345b09317f81e98b535643c58c93601b71d7d7d7
Michael Wilson <pianoplayer868@hotmail.com>
2024-05-20 13:55:34 -0700

    Added a simple filter function to filter the dog breeds while maintaining the sub category of the dogs

--- src/AllDogsRenderer.tsx
+++ src/AllDogsRenderer.tsx
@@ -4,10 +4,23 @@ import { NestedListRenderer } from "./ListRenderer";
 import { GetAllDogs } from "./Utils";
 import { NestedArrayEl } from "./Constants";
 
-export function AllDogsRenderer() {
-    const [allDogs, setAllDogs] = useState([] as NestedArrayEl[]);
+export interface AllDogsRendererProps {
+    queryFilter?: string;
+}
+
+export function AllDogsRenderer({queryFilter}: AllDogsRendererProps) {
+    const [allDogs, setAllDogs] = useState([] as NestedArrayEl[]); 
     useEffect(() => {
-        GetAllDogs().then((dogList: NestedArrayEl[]) => setAllDogs(dogList));
-    }, []);
+        GetAllDogs().then((dogList: NestedArrayEl[]) => {
+            if (queryFilter) {
+                setAllDogs(dogList.filter((dog, index) => {
+                    const isArrayAndKeep = Array.isArray(dog) && (dogList[index - 1] as string).startsWith(queryFilter);
+                    return isArrayAndKeep || (typeof dog === 'string' && (dog as string).startsWith(queryFilter));
+                }));
+            } else {
+                setAllDogs(dogList)
+            }   
+        });
+    }, [queryFilter]);
     return <NestedListRenderer listContent={allDogs} />
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { AllDogsRenderer } from "./AllDogsRenderer";
 // import { IMAGE_TYPES } from "./Constants";
 // import { ImageRenderer } from "./ImageRenderer";
 function App() {
   // return <ImageRenderer imageType={IMAGE_TYPES.DOG} />;
-  return <AllDogsRenderer />
+  const [filter, setFilter] = useState('');
+  useEffect(() => setFilter(filter), [filter]);
+  return (
+    <>
+      <label htmlFor='dogFilter'>Filter by breed</label><br />
+      <input id='dogFilter' type='text' onChange={(event) => setFilter(event.target.value)} />
+      <AllDogsRenderer queryFilter={filter} />
+    </>);
 }
 
 export default App;
